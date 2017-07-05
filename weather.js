6f6ea53566861e64a731db359867b57023eb632d
Guillermo <devivero@gmail.com>
2017-07-05 13:57:05 -0700

    did homework assigment #6

--- weather.js
+++ weather.js
@@ -1,25 +1,34 @@
 const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?"//"https://uwpce-fortune-proxy.herokuapp.com/"
-const form = document.getElementById("apiOptions")
-// const apiURL = "https://uwpce-fortune-proxy.herokuapp.com/?format=html"
+//const form = document.getElementById("apiOptions")
 let debug = null
 
-function handleSubmit () {
-  event.preventDefault()
-  console.log(form)
+function wow(){
+  // get user's location from the browser
+  navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+}
+function geolocSuccess(position){
+  //const newPos = {lat: position.coords.latitude, lng: position.coords.longitude};
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let newPosString = "lat=" + lat + "&lon=" + lon + "&units=imperial" + "&APPID=32f2896d2c86b6cab0880ccfda804d0c";
+  getWeather(newPosString);
+}
 
-
-  /*
-  // get form values
-  let values = {format: form.format.value}
-  // serialize them into a query string
-  let queryString = queryBuilder(values)
-  // call getQuote with the query string
-  */
-  getWeather(queryString)
+// callback for no success getting location from user's browser
+function geolocError(){
+  console.log("Error getting user's location :(");
+  let weatherDiv = document.getElementById("weather")
+  weatherDiv.innerHTML = "Error getting user's location :("
 }
 
 function getWeather (queryString) {
   let request = new XMLHttpRequest()
+
+  if (queryString == "seattle"){
+    queryString = "lat=47.6762&lon=-122.3182&units=imperial&APPID=32f2896d2c86b6cab0880ccfda804d0c"
+  } else if (queryString == "london"){
+    queryString = "lat=51.5074&lon=0.1278&units=imperial&APPID=32f2896d2c86b6cab0880ccfda804d0c"
+  }
 
   // starts talk to API - 3 params
   // request method, url, (optional) async flag (default true)
@@ -41,31 +50,13 @@ function getWeather (queryString) {
   }
 
   // fires if something goes wrong
-  request.error = function (errorObject) {
+  request.onerror = function (errorObject) {
     console.log("broken :(")
     console.log(errorObject)
+    let weatherDiv = document.getElementById("weather")
+    weatherDiv.innerHTML = "broken :("
   }
 
   // send the request!
   request.send()
 }
-
-function queryBuilder(queryObj){
-  let holder = []
-  // loop through queryObj key value pairs
-  for(let key in queryObj){
-    // turn each one into "key=value"
-    let convert = `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key])}`
-    // encodeURIComponent converts spaces and & to URI friendly values so we don't have to worry about them
-    holder.push(convert)
-  }
-  // concatenate the pairs together, with & between
-  let longString = holder.join("&")
-  // prepend a ? to concatenated string, return
-  return `?${longString}`
-}
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let btnQuote = document.querySelector("button")
-//   btnQuote.addEventListener("click", getQuote)
-// })
